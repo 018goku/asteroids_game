a442d171f42135574ce2ebf14636ed097dd83027
018goku <sacaychom@gmail.com>
2025-06-15 08:52:55 +0300

    B:draw_player

--- development/main.py
+++ development/main.py
@@ -2,22 +2,32 @@ import sys
 import pygame
 from pygame.color import THECOLORS
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    screen.fill(THECOLORS["black"])
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+                return
+
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
