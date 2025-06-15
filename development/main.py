60b66e60f00bd3fe72343b2c19c3ad9b956924a5
018goku <sacaychom@gmail.com>
2025-06-15 05:45:39 +0300

    A:first_part

---
+++ development/main.py
@@ -0,0 +1,24 @@
+import sys
+import pygame
+from pygame.color import THECOLORS
+from constants import *
+
+def main():
+    pygame.init()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.fill(THECOLORS["black"])
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
